3c21396339cdebc99f488dc43daf1212c4536801
shahrodkh <shahrodkh@gmail.com>
2017-03-25 15:43:48 -0700

    added webccv library to load the webassembly module

--- demo/server.js
+++ demo/server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const app = express();
 
 app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
